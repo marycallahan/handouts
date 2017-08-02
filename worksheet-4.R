6bf3f5f51952c3323e1849a7aa30e6dc9f15d996
marycallahan <mcallah2@umd.edu>
2017-08-02 15:11:25 -0400

    finished all lessons

--- worksheet-4.R
+++ worksheet-4.R
@@ -10,30 +10,61 @@ response <- data.frame(
 
 library(tidyr)
 
-df <- gather(...)
+df <- gather(response, key="factor",value="response", -trial)
 
 counts <- data.frame(
-  site = ...,
-  species = ...,
+  site = rep(1:3, each = 2),
+  species = rep(c("lynx", "hare"), 3),
   n = c(2, 341, 7, 42, 0, 289)
 )
 
-counts_spread <- ...(counts,
-			...,
-			...)
+counts_spread <- spread(counts,
+			key=species,
+			value=n)
 
 ## Exercise 1
-
-...
+df2<-counts[-5,]
+#with brackets, everything is row,column. So leaving column blank means 
+#take away everything that is in that row
+spread(df2, key=species, value=n)
+#turns your zero into NA
+spread(df2, key=species, value=n, fill=0)
+#default value for missing data is NA, fill tells you what to fill all teh blanks in as 
+# so we say fill with zero and get the zero value back 
 
 ## Read comma-separated-value (CSV) files
 
-animals <- ...
+animals <- read.csv('data/animals.csv')
+#someting didnt read in right about this data - the factor for species ID has an empty string 
+#in the first value. There is a blank in the data, and it's reading it as blank 
+# but we want to read it in as an NA
 
-animals <- read.csv('data/animals.csv', )
+animals <- read.csv('data/animals.csv', na.strings='')
+#na.strings is the command to treat whatever is after the equal sign as a NA
+#we have empty quotations to indicate it's the blank
+#check the structure and you see that the blank is gone
+#the na.strings is flexible, can put in a vector of arguments (things) to treat as NA
+
+# now looking at Key Data Wrianging Funcions
+#dplyr is the R library for data wrangling
+#filter is probabl the most important one - filters out rows that we don't want (like filter out
+#everything of one species that wedont want to look at)
+#select can keeeep the columns you want
+
+#arrange sorts data
+#mutate creates a new column that's a function of other columns in the data frame
+#ex have column that's weight in grams, use mutate to create column that is weight
+#in kg, so divides grams column by 1000
+#on lesson variable is surrounded by %- that just means type in a name for the variable
+#group - chunk data into groups and idividually process by grouns
+#ex. take data fram with all the species, can group by species ID so when you 
+#summarize the data, it runs a summary gorup by group - summarizes (ex with mean)
+#by species instead of a mean of the whole data frame together
 
 library(dplyr)
-library(...)
+#red is not always an error - so the masking is ok
+#if red is an error, it will actually say error
+#skip down to line 76
 
 con <- ...(..., host = 'localhost', dbname = 'portal')
 animals_db <- ...
@@ -43,18 +74,45 @@ dbDisconnect(...)
 ## Subsetting and sorting
 
 library(dplyr)
-animals_1990_winter <- filter(...,
-                              ...,
-                              ...)
+#lets use filter function on the animals data frame to just look at 1990 data
+animals_1990_winter <- filter(animals,
+                              year == 1990,
+                              month%in%1:3)
+#year is a logical test- double equal sign means "has to be equal to"
+#want all winter months, and in this data set, month is indicated by a number, 
+#so we ask for month "in" the range of 1:3
+#look over at enviroment and we can see that new animals_1990_winter has 491 obs
+#while animals has 35549, so we know our filter worked
 
-animals_1990_winter <- select(animals_1990_winter, ...)
+#ok now we don't need the year column because they're all 1990
+#so use select to pick the columns we want.
+#except that a lot to tyoe
+#so
+animals_1990_winter <- select(animals_1990_winter, -year
+                              )
+str(animals_1990_winter)
+#our variable dropped from 9 variables to 8. yay!
+#but if you might want to go back to the 9 variable value, make sure
+#you define the select as a new variable, not the same name
 
-sorted <- ...(animals_1990_winter,
-              ...)
+#now lets rearrange the data frame so it's sorted by columns
+#sort first by species ID, then by weight
 
+sorted <- arrange(animals_1990_winter,
+              desc(species_id), weight)
+#desc is helper function to sort by descending order (z to A instead of A to z,
+#which would be ascend (idk what the command is but maybe fill in the blank?))
+#wantto see the beginning of the data frame but not the whole thing, use head
+head(sorted)
+
+#view will pop out the whole data table as a new tab here MUST BE CAPITAL V
+View(sorted)
 ## Exercise 2
+#Write code that returns the id, sex and weight of all surveyed
+#individuals of Reithrodontomys montanus (RO).
 
-...
+RO <- filter(animals, species_id == 'RO')
+RO2<-select (RO, id, sex, weight)
 
 ## Chainning with pipes
 
@@ -103,3 +161,12 @@ pivot <- ...
   spread(month, prop, fill = 0)
 
 dbDisconnect(con)
+
+
+
+library(tidyr)
+df3<-gather(response, "factor", "repsonse", -trial)
+df3
+
+
+dss3<-gather(dss2,"factor", "response",-)
--- worksheet-5.R
+++ worksheet-5.R
@@ -1,47 +1,52 @@
 ## Getting started
 
 library(dplyr)
-library(...)
-animals <- read.csv(..., na.strings = '') %>%
+library(ggplot2)
+animals <- read.csv('data/animals.csv', na.strings = '') %>%
   filter(!is.na(species_id), !is.na(sex), !is.na(weight))
+#%>% = take the result of the read.csv command and sends it immediately into the filter command
+
 
 ## Constructing layered graphics in ggplot
 
-ggplot(...,
-       ...) +
+ggplot(data=animals, aes(x=species_id, y=weight))+ geom_point()
   ...
-
+#constructs a scatter plot
+  
+ggplot(data = animals,
+       aes(x = species_id, y = weight)) +geom_boxplot()
+  ...
+# construct a box plot
+  
 ggplot(data = animals,
        aes(x = species_id, y = weight)) +
-  ...
+  geom_boxplot() +
+  geom_point(stat='summary',fun.y='mean',color='red')
 
 ggplot(data = animals,
-       aes(x = species_id, y = weight)) +
-  geom_boxplot() ...
-  geom_point(...,
-             ...,
-             ...)
-
-ggplot(data = animals,
-       aes(x = species_id, y = weight, ...)) +
+       aes(x = species_id, y = weight, color=species_id)) +
   geom_boxplot() +
   geom_point(stat = 'summary',
              fun.y = 'mean')
 
 ## Exercise 1
+#Using dplyr and ggplot show how the mean weight of individuals of the 
+#species DM changes over time, with males and females shown in different colors.
 
-...
+animals_dm<-filter(animals,species_id=="DM")
+ggplot(data=animals, aes(x=weight, y=year, color=sex))+geom_line(stat='summary',fun.y='mean')
 
 ## Adding a regression line
+ggplot(data=animals, aes(x=weight, y=year, color=sex))+geom_point(stat='summary',fun.y='mean')
 
 levels(animals$sex) <- c('Female', 'Male')
-animals_dm <- filter(animals, ...)
-ggplot(...,
+
+ggplot(data=animals_dm,
        aes(x = year, y = weight)) +
-  geom_point(...,
+  geom_point(aes(shape=sex),
              size = 3,
              stat = 'summary',
-             fun.y = 'mean') +
+             fun.y = 'mean') + geom_smooth(method='lm')
   ...
 
 ggplot(data = animals_dm,
@@ -50,12 +55,10 @@ ggplot(data = animals_dm,
              size = 3,
              stat = 'summary',
              fun.y = 'mean') +
-  geom_smooth(...)
+  geom_smooth(aes(group=sex), method="lm")
 
 ggplot(data = animals_dm,
-       aes(...,
-           ...,
-           ...) + 
+       aes(x=year, y=weight, color=sex)) + 
   geom_point(aes(shape = sex),
              size = 3,
 	           stat = 'summary',
@@ -63,7 +66,7 @@ ggplot(data = animals_dm,
   geom_smooth(method = 'lm')
 
 # Storing and re-plotting
-
+#store the plot as a variable, can check by doing str(year_wgt)
 year_wgt <- ggplot(data = animals_dm,
                    aes(x = year,
                        y = weight,
@@ -74,26 +77,39 @@ year_wgt <- ggplot(data = animals_dm,
              fun.y = 'mean') +
   geom_smooth(method = 'lm')
 
-year_wgt +
+year_wgt +scale_color_manual(values=c('dark blue','orange'))
   ...
                      
 year_wgt <- year_wgt +
-  scale_color_manual(...)
+  scale_color_manual(values=c('dark blue','orange'))
+
+#can clear plot history by clicking on the broom, then just type
+#year_wgt
+#and see the plot
 year_wgt
 
 ## Exercise 2
 
-...
+#Create a histogram, using a geom_histogram() layer, of the weights of 
+#individuals of species DM and divide the data by sex. Note that instead 
+#of using color in the aesthetic, you’ll use fill to distinguish the 
+#sexes. Also open the help with ?geom_histogram and determine how to 
+#explicitly set the bin width.
+ggplot(data=animals_dm, aes(x=weight,fill=sex))+
+geom_histogram(binwidth=1)
+
+#binwwidth of 1 shows teh counts for each gram of weight
+#bindwitch of 10 would group the counts by 10 gram chunks
 
 ## Axes, labels and themes
 
 histo <- ggplot(data = animals_dm,
                 aes(x = weight, fill = sex)) +
-  geom_...
+  geom_histogram(binwidth=3, color='white')
 histo
 
 histo <- histo +
-  ...(title = 'Dipodomys merriami weight distribution',
+  labs(title = 'Dipodomys merriami weight distribution',
        x = 'Weight (g)',
        y = 'Count') +
   scale_x_continuous(limits = c(20, 60),
@@ -103,26 +119,30 @@ histo
 histo <- histo +
   theme_bw() +
   theme(legend.position = c(0.2, 0.5),
-        plot.title = ...,
-        ... = element_text(...),
-        ... = element_text(size = 13, vjust = 0))
+        plot.title = element_text(face="bold", vjust=2),
+        axis.title.x = element_text(size=13, vjust=1),
+        axis.title.y = element_text(size = 13, vjust = 0))
+histo
+
+#vjust=  vertical justification
 histo
 
 ## Facets
 
-animals_common <- filter(animals, ...)
-ggplot(data = ...,
-       ...) +
+animals_common <- filter(animals, species_id %in% c("DM", "PP", "DO"))
+ggplot(data = animals_common, aes(x=weight)) +
   geom_histogram() +
-  ...
+  facet_wrap(~species_id)+
   labs(title = "Weight of most common species",
        x = "Count",
        y = "Weight (g)")
 
+#but how does each of these compare to the entire distribution
+#add entire distribution as another layer on each of these plots
 ggplot(data = animals_common,
        aes(x = weight)) +
-  geom_histogram(...,
-                 ...) +
+  geom_histogram(data=select(animals_common, -species_id),
+                 alpha=0.2) +
   geom_histogram() +
   facet_wrap( ~ species_id) +
   labs(title = "Weight of most common species",
@@ -130,15 +150,26 @@ ggplot(data = animals_common,
        y = "Weight (g)")
 
 ggplot(data = animals_common,
-       aes(x = weight, ...)) +
-  geom_histogram(...) +
+       aes(x = weight, fill=species_id)) +
+  geom_histogram(aes(y=..density..)) +
   facet_wrap( ~ species_id) +
   labs(title = "Weight of most common species",
-       x = "Count",
-       y = "Weight (g)") +
+       x = "weight (g)",
+       y = "count") +
   guides(fill = FALSE)		
 
 ## Exercise 3
+#The formula notation for facet_grid (different from facet_wrap) 
+#interprets left-side variables as one axis and right-side variables 
+#as another. For these three common animals, create facets in the 
+#weight histogram along two categorical variables, with a row for 
+#each sex and a column for each species.
 
-...
+ggplot(data = animals_common,
+       aes(x = weight, fill=species_id:sex)) +
+  geom_histogram() +
+  facet_grid(sex ~ species_id) +
+  labs(title = 'Weight of common species by sex',
+       x = 'Count',
+       y = 'Weight (g)')
 
